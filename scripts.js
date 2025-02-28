df5e59c4d1d678faf501b359bb2fb5a09d5a1882
GH-KB-code <153880858+GH-KB-code@users.noreply.github.com>
2025-02-28 17:15:54 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,181 @@
+// Sample data for covenants
+const covenantData = [
+    {
+        id: 'covenant1',
+        title: 'Edenic Covenant',
+        imgSrc: 'https://miro.medium.com/v2/resize:fit:2000/1*rJ36ymwzBt1IJyHyMcSfHQ.jpeg',
+        description: `
+        At the very beginning, God created the heavens and the earth, placing mankind in the perfect setting— the Garden of Eden. The **Edenic Covenant** was a simple yet profound agreement. Adam and Eve were to enjoy the paradise of Eden, which was filled with beauty and abundance. They were commanded to be fruitful, multiply, fill the earth, and have dominion over all creation.
+        
+        However, this covenant had one condition: they were to remain obedient to God's command and not eat from the tree of the knowledge of good and evil. This was not a cruel restriction but a safeguard for their relationship with God. By obeying, they would continue in perfect communion with Him.
+        
+        Unfortunately, Adam and Eve chose to disobey, and this breach of covenant brought sin, death, and separation from God into the world. Despite their disobedience, God showed mercy and love. He promised that one day, the "seed of the woman" would crush the serpent's head— a prophecy signaling God's intention to redeem humanity.
+        `,
+        audioSrc: 'audio/edenic_recording.m4a',
+        
+    },
+
+    {
+        id: 'covenant2',
+        title: 'Adamic Covenant',
+        imgSrc: 'https://edge.mcsw.net/mcsweeneys/17351jn22j6xbstkty8plqoygpm7',
+        description: `
+            Following the fall of Adam and Eve, God issued the Adamic Covenant,
+            which was not a new promise but a continuation of the covenant of grace
+            in the face of sin. 
+            
+            This covenant brought the reality of consequences for
+            disobedience: pain in childbirth for Eve, a cursed ground for Adam, hard
+            work, and spiritual death for both. However, even in the midst of judgment,
+            God extended a glimmer of hope. The promise of the future seed of the woman
+            to defeat the serpent marked the beginning of God's redemptive plan.
+            While sin would reign in the world, God's ultimate intention was to restore
+            what had been lost.
+        `,
+        audioSrc: 'audio/adamic_recording.m4a',
+        
+    },
+    {
+        id: 'covenant3',
+        title: 'Noahic Covenant',
+        imgSrc: 'https://en.brightchurch-it.org/wp-content/uploads/2019/07/002-%E6%8C%AA%E4%BA%9A%E4%B8%80%E5%AE%B6%E5%85%AB%E5%8F%A3%E7%9C%8B%E5%88%B0%E5%BD%A9%E8%99%B9%E6%AC%A2%E5%91%BC%E9%9B%80%E8%B7%83-ZB-20171208.jpg',
+        description: `
+            As humanity continued to sin, the world became more corrupt. God decided to cleanse the Earth through a great flood, 
+            saving only Noah and his family. After the flood, God made the Noahic Covenant with Noah 
+            and his descendants, as well as every living creature on the Earth. 
+        
+            This covenant was a promise that God would never again destroy the earth with a flood. 
+            The rainbow became the symbol of the covenant, the visible sign of God’s faithfulness to His promise. 
+        
+            While the Noahic Covenant did not remove the problem of sin, it showed God’s mercy. 
+            It demonstrated that although sin on the earth remained, God was committed to preserving life 
+            and working through human history to bring about His plan of salvation. 
+        
+            The Noahic Covenant was a fresh start for humanity, with the command to be fruitful and multiply still intact.
+        `,
+        audioSrc: 'audio/noahic_recording.m4a',
+        
+    },
+    {
+        id: 'covenant4',
+        title: 'Abrahamic Covenant',
+        imgSrc: 'https://static.wixstatic.com/media/00f46b_7e80f9161e114ac8bf1e9912cad519ad~mv2.jpg/v1/fill/w_926,h_509,al_c,lg_1,q_85/00f46b_7e80f9161e114ac8bf1e9912cad519ad~mv2.jpg',
+        description: `The next significant step in God’s redemptive plan was His covenant with Abraham. 
+                        God called Abraham from his homeland and promised to make him the father of a great nation. 
+                        This was the Abrahamic Covenant, which contained 3 main promises: land, numerous descendants, 
+                        and a great blessing to all nations through Abraham’s lineage. 
+
+                        This covenant set the stage for Israel’s future as a nation. More significantly, 
+                        God promised that through Abraham’s seed, all the nations of the earth would be blessed. 
+                        This promise would ultimately be fulfilled in the person of Jesus Christ, 
+                        who would bring the ultimate blessing of salvation to the world.`,
+        audioSrc: 'audio/abrahamic_recording.m4a',
+    },
+
+    {
+        id: 'covenant5',
+        title: 'Mosaic Covenant',
+        imgSrc: 'https://91b6be3bd2294a24b7b5-da4c182123f5956a3d22aa43eb816232.ssl.cf1.rackcdn.com/contentItem-10637658-97768736-bh0x8w0hxlm35-or.png',
+        description: `
+                    The Mosaic Covenant was established after God delivered the Israelites from slavery in Egypt. 
+                    At Mount Sinai, God gave the law to Moses, which included the Ten Commandments and a detailed system of laws for Israel to follow.
+                     This covenant was conditional. If Israel obeyed God’s commands, then they would be His treasured possession, a kingdom of priests, 
+                     and a holy nation. The Mosaic Covenant set apart Israel as a special people, distinct from all other nations. 
+                     It highlighted God’s holiness and the need for purity and obedience. 
+                     
+                     However, it also showed that humanity was not always able to perfectly follow God’s laws. 
+                     The law acted as a mirror, showing Israel their sin and their need for a Savior. 
+                     The Mosaic Covenant was not the end of God’s promises but a stepping stone toward the fulfillment of His redemptive plan. 
+                     It foreshadowed Christ, who came not to abolish the law but to fulfill it perfectly.`,
+        audioSrc: 'audio/mosaic_recording.m4a',
+    },
+    {
+        id: 'covenant6',
+        title: 'Davidic Covenant',
+        imgSrc: 'https://www.jesuswalk.com/psalms/images/gerrit-van-honthorst-king-david-playing-the-harp-1611-1156x1407x300.jpg',
+        description: `
+                    God’s covenant with David was a turning point. The Davidic covenant promised that David’s descendant would rule over Israel forever. 
+                    The kingdom hat would never end. This was fulfilled in Jesus who reigns as eternal King. 
+                    The Davidic covenant pointed to the Messianic hope in christ whose reign would not be limited to an earthly kingdom but also to an eternal kingdom, 
+                    ushering in God’s eternal rule over all creation. Through David’s lineage, God promises a savior who establishes God’s Kingdom, forever.`,
+        audioSrc: 'audio/davidic_recording.m4a',
+    },
+    {
+        id: 'covenant7',
+        title: 'New Covenant',
+        imgSrc: 'https://i.pinimg.com/736x/54/b0/19/54b019c9aa38f7347178720c359b1f3b.jpg',
+        description: `The New covenant was promised by God to the prophet Jeremiah and it was established by Jesus at the Last Supper. 
+                        In this covenant, God promised to forgive the sins of His people and to write His laws on their hearts. 
+                        The New covenant is unconditional. It promises forgiveness and eternal life through the sacrifice of Jesus Christ. 
+                        This covenant is not based on keeping the law but on faith on Jesus Christ who is the mediator of this covenant. 
+                        The new covenant was fulfilled in Jesus Christ whose death and resurrection made the forgiveness of sin available to all. 
+                        This is the ultimate covenant, offering salvation through faith in christ and not by works in guaranteeing eternal life.`,
+        audioSrc: 'audio/new_recording.m4a',
+    },
+    {
+        id: 'covenant8',
+        title: 'Eternal Covenant',
+        imgSrc: 'https://i.swncdn.com/media/1280w/via/images/2023/10/16/33048/33048-istockgetty-images-plusnorima_source_file.jpg',
+        description: `The final covenant known as the eternal covenant was the secured through the death and resurrection of Jesus Christ.
+                     This covenant signifies the completion and fulfillment of all previous covenants. It is eternal, because Jesus Christ’s sacrifice is meant to be 
+                     for all time, offering eternal redemption for those who believe in him. In the eternal covenant, all those who trust in Jesus are meant to reconcile with God forever. 
+                     Jesus is perfect and complete. Through this covenant, believers are promised eternal life and an unbreakable relationship with God.`,
+        audioSrc: 'audio/eternal_recording.m4a',
+    }
+];
+
+let currentIndex = 0;
+
+function renderCovenantList() {
+    const listContainer = document.getElementById('covenantList');
+    listContainer.innerHTML = ''; // Clear the current content
+
+    covenantData.forEach((covenant, index) => {
+        const li = document.createElement('li');
+        li.classList.add('covenant-item');
+        li.setAttribute('onclick', `showCovenantDetails(${index})`);
+
+        const div = document.createElement('div');
+        div.classList.add('circle');
+        const img = document.createElement('img');
+        img.src = covenant.imgSrc;
+        img.alt = covenant.title;
+        div.appendChild(img);
+
+        const h3 = document.createElement('h3');
+        h3.textContent = covenant.title;
+
+        li.appendChild(div);
+        li.appendChild(h3);
+        listContainer.appendChild(li);
+    });
+}
+
+
+
+function showCovenantDetails(index) {
+    const detailsSection = document.getElementById('details');
+    const detailsDiv = document.getElementById('covenantDetails');
+    const covenant = covenantData[index];
+
+    detailsDiv.innerHTML = `
+        <h3>${covenant.title}</h3>
+        <p>${covenant.description}</p>
+        ${covenant.audioSrc ? `<button class="audio-button" onclick="playAudio(${index})">Click to Hear My Voice</button>` : ''}
+        <audio id="audio${index}" src="${covenant.audioSrc}"></audio>
+    `;
+    detailsSection.classList.remove('hidden');
+}
+
+function playAudio(index) {
+    const audio = document.getElementById(`audio${index}`);
+    audio.play();
+}
+
+function closeDetails() {
+    document.getElementById('details').classList.add('hidden');
+}
+
+
+// Initialize the carousel
+renderCovenantList();
